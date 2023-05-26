ed735cb1eba421a025cdb04e6744a2003b415509
Vinícius Rampelotti <viniciusrampeloti@gmail.com>
2023-05-26 14:10:17 -0300

    .

---
+++ aula2/exercicio-basico.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é Vinícius. Estou aprendendo JavaScript às', 2,'horas da tarde.')
---
+++ aula2/index.js
@@ -0,0 +1,6 @@
+console.log('Vinícius') //string definido automaticamente
+console.log("Vinícius")
+console.log(`Vinícius`)
+
+console.log(123456, 15.85, "Vinícius") //valores literais não necessitam de aspas
+
