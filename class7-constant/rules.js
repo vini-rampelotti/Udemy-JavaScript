a3b26d889935cbd523b0b898da8bd92be3df2f41
Vinícius Rampelotti <viniciusrampeloti@gmail.com>
2023-07-04 16:34:41 -0300

    Create rules.js

---
+++ class7-constant/rules.js
@@ -0,0 +1,12 @@
+// You cannot create variables with reserved words:
+
+// Variables need to have signifcatives names:
+
+// Can't start a variable name with a number:
+
+//Can't have any space or special chars:
+
+//Use camelCase:
+
+// Vars are case sensitive:
+// nomecliente != nomeCliente
