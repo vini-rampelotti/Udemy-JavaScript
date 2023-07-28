ccc55bab9adc044e0a403e140b33dd4e0822216d
Vinícius Rampelotti <viniciusrampeloti@gmail.com>
2023-07-28 01:08:42 -0300

    constantes

---
+++ class7-constant/index.js
@@ -0,0 +1,10 @@
+const firstNumber = 5;
+const secondNumber = 10;
+const result = firstNumber * secondNumber;
+const doubleResult = result * 2;
+
+let tripleResult = result * 3;
+
+tripleResult = tripleResult + 5;
+
+console.log(tripleResult);
